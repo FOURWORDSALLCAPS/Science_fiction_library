20c903fee73dc7f6cb293f592b759b05e6d73ba3
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-06-04 23:02:26 +0300

    Download book covers

--- script.py
+++ script.py
@@ -1,9 +1,8 @@
 import requests
 import os
-from pathlib import Path
 from pathvalidate import sanitize_filename, sanitize_filepath
 from bs4 import BeautifulSoup
-
+from urllib.parse import urljoin, urlparse
 
 def check_for_redirect(response):
     if response.url == 'https://tululu.org/':
@@ -29,9 +28,8 @@ def download_txt(url, book_id, folder='books/'):
         title, author = title_author.split(" :: ")
         filename = sanitize_filename(f"{title.strip()}")
         folder = sanitize_filepath(folder)
-
+        book_image_url = urljoin(url, soup.find('div', class_='bookimage').find('img')['src'])
         filepath = os.path.join(folder, f"{book_id}. {filename}.txt")
-
         book_text_url = f"{url}txt.php?id={book_id}"
         book_text_response = requests.get(book_text_url, allow_redirects=False)
         check_for_redirect(book_text_response)
@@ -41,11 +39,29 @@ def download_txt(url, book_id, folder='books/'):
         with open(filepath, "w", encoding="utf-8") as file:
             file.write(book_text)
 
+        return book_image_url
     except requests.exceptions.HTTPError as e:
         print(f"Error: {e}")
-        return None, None
+
+
+def download_image(url, folder='images/'):
+    try:
+        os.makedirs(folder, exist_ok=True)
+        response = requests.get(url)
+        response.raise_for_status()
+
+        filename = os.path.basename(urlparse(url).path)
+        filepath = os.path.join(folder, filename)
+        with open(filepath, 'wb') as file:
+            file.write(response.content)
+
+    except requests.exceptions.HTTPError as e:
+        print(f"Ошибка HTTP: {e}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка запроса: {e}")
 
 
 url = 'https://tululu.org/'
 for book_id in range(11):
-    download_txt(url, book_id=book_id)
+    download_image(download_txt(url, book_id=book_id))
