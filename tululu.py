99b3d8b4a0416bd1fde911515d72d87dc34b1546
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-06-14 13:22:48 +0300

    Download books from all pages

--- tululu.py
+++ tululu.py
@@ -10,7 +10,7 @@ from urllib.parse import urljoin, urlparse
 
 
 def check_for_redirect(response):
-    if response.url == 'https://tululu.org/':
+    if response.url == 'https://tululu.org/l55/':
         raise requests.HTTPError('Page redirected')
 
 
@@ -20,7 +20,7 @@ def download_txt(title, book_id, folder='books/'):
     os.makedirs(folder, exist_ok=True)
     filename = sanitize_filename(f"{title}")
     folder = sanitize_filepath(folder)
-    filepath = os.path.join(folder, f'{book_id}. {filename}.txt')
+    filepath = os.path.join(folder, f'{filename}.txt')
     book_text_response = requests.get(url, params=params)
     check_for_redirect(book_text_response)
     book_text_response.raise_for_status()
@@ -74,23 +74,31 @@ def main():
     parser.add_argument('--end_id', type=int, default=20,
                         help='id книги, на которой закончится скачивание')
     args = parser.parse_args()
-    for book_id in range(args.start_id, args.end_id):
-        book_page_url = f'https://tululu.org/b{book_id}'
-        try:
-            book_page_response = requests.get(book_page_url)
-            check_for_redirect(book_page_response)
-            book_page_response.raise_for_status()
-            soup = BeautifulSoup(book_page_response.text, 'lxml')
-            book = parse_book_page(soup, book_page_url)
-            download_txt(book['title'], book_id=book_id)
-            download_image(book['image_url'])
-            print('Название:', book['title'])
-            print('Автор:', book['author'])
-        except requests.exceptions.HTTPError as e:
-            print(f'Error: Unable to download book {book_id}: {e}', file=sys.stderr)
-        except requests.exceptions.ConnectionError as e:
-            print(f'The request for book {book_id} failed: {e}', file=sys.stderr)
-            time.sleep(3)
+    for page in range(args.start_id, args.end_id):
+        url = f"https://tululu.org/l55/{page}"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        for book in soup.find_all('div', class_='bookimage'):
+            book_href = book.find('a')['href']
+            trans_table = {ord('b'): None}
+            book_id = sanitize_filename(book_href).translate(trans_table)
+            book_link = urljoin(url, book_href)
+            url = f"{book_link}"
+            try:
+                response = requests.get(url)
+                check_for_redirect(response)
+                response.raise_for_status()
+                soup = BeautifulSoup(response.text, 'lxml')
+                book = parse_book_page(soup, url)
+                download_txt(book['title'], book_id=book_id)
+                download_image(book['image_url'])
+                print('Название:', book['title'])
+                print('Автор:', book['author'])
+            except requests.exceptions.HTTPError as e:
+                print(f'Error: Unable to download book {book_id}: {e}', file=sys.stderr)
+            except requests.exceptions.ConnectionError as e:
+                print(f'The request for book {book_id} failed: {e}', file=sys.stderr)
+                time.sleep(3)
 
 
 if __name__ == '__main__':
