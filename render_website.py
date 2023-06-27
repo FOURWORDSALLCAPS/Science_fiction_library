c8d90e620fca4dea8da8f385717a394301975547
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-06-27 14:10:31 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,6 +1,6 @@
 import json
 
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from livereload import Server, shell
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
@@ -15,14 +15,20 @@ env = Environment(
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-template = env.get_template('template.html')
 
-rendered_page = template.render(
-    books=json.loads(books_json)
-)
+def on_reload():
+    template = env.get_template('template.html')
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    rendered_page = template.render(
+        books=json.loads(books_json)
+    )
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+if __name__ == '__main__':
+    on_reload()
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
