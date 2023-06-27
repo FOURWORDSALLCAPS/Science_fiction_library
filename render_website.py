27a17d81b251de5522f8a8ef3f84a5c9c8af6837
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-06-27 15:37:56 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,7 +1,8 @@
 import json
 
-from livereload import Server, shell
+from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from math import ceil
 
 
 with open("json/book.json", "r", encoding="utf-8") as file:
@@ -17,14 +18,20 @@ env = Environment(
 
 
 def on_reload():
-    template = env.get_template('template.html')
+    page_size = 20
 
-    rendered_page = template.render(
-        books=json.loads(books_json)
-    )
+    num_pages = ceil(len(books) / page_size)
+    for page_num in range(1, num_pages + 1):
+        start_idx = (page_num - 1) * page_size
+        end_idx = start_idx + page_size
+        page_books = books[start_idx: end_idx]
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+        template = env.get_template('template.html')
+
+        rendered_page = template.render(books=page_books)
+
+        with open(f'pages/index{page_num}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 if __name__ == '__main__':
