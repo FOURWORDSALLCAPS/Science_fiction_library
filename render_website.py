c5453a32a1c71d1e2fbd6934850539213693dc31
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-06-30 11:35:34 +0300

    Corrected variable name

--- render_website.py
+++ render_website.py
@@ -7,12 +7,12 @@ from math import ceil
 
 
 def on_reload(books, env):
-    page_size = 20
+    page_limit = 20
 
-    num_pages = ceil(len(books) / page_size)
+    num_pages = ceil(len(books) / page_limit)
     for page_num in range(1, num_pages + 1):
-        start_idx = (page_num - 1) * page_size
-        end_idx = start_idx + page_size
+        start_idx = (page_num - 1) * page_limit
+        end_idx = start_idx + page_limit
         page_books = books[start_idx: end_idx]
 
         template = env.get_template('template.html')
